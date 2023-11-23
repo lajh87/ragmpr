bfd3bd74e555238dfff4b56f12ed8749b4e5a1d9
lheley <lajh87@live.co.uk>
2023-11-23 08:06:13 +0000

    working similiarity search from pinecone database

--- vectordatabase.py
+++ vectordatabase.py
@@ -13,3 +13,39 @@ pinecone.init(
 # Connect to Index
 index = pinecone.GRPCIndex(index_name)
 index.describe_index_stats()
+
+
+query = "Provide an example of a major project"
+
+index.similarity_search(
+    query,  # our search query
+    k=3  # return 3 most relevant docs
+)
+
+# Open Embeddings
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+model_name = 'text-embedding-ada-002'
+embed = OpenAIEmbeddings(
+    model=model_name,
+    openai_api_key=os.getenv("OPENAI_API_KEY")
+)
+
+# Switch Back to Langchain
+from langchain.vectorstores import Pinecone
+text_field = "text"
+
+# switch back to normal index for langchain
+index = pinecone.Index(index_name)
+
+vectorstore = Pinecone(
+    index, embed.embed_query, text_field
+)
+
+# Query
+query = "Provide an example of a major project"
+vectorstore.similarity_search(
+    query,  # our search query
+    k=3  # return 3 most relevant docs
+)
